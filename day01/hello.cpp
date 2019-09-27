d892a2195b9557d6d494f7dec03ab5065d20695a
alexcf <1455996286@qq.com>
2019-09-27 22:10:27 +0800

    222

--- day01/hello.cpp
+++ day01/hello.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include<windows.h>
+#include<stdlib.h>
 using namespace std;
 int main(){
     cout << "hello world" << endl;
+    cout << "ÄãºÃ" << endl;
     system("pause");
 }
